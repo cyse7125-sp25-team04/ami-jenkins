14fc811f2e3c0928af3ff28c016b058f92307e63
sri-vijay-kalki <srivijaykalki@gmail.com>
2025-01-30 01:14:15 -0500

    [FIX]: fix domain name, ssh_username

--- packer/scripts/setup-jenkins-ngnix.sh
+++ packer/scripts/setup-jenkins-ngnix.sh
@@ -7,7 +7,7 @@ sudo apt-get install nginx -y
 sudo systemctl status nginx
 
 # Install Java
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 
 # Install Jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -17,8 +17,10 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+# Install  Let's Encrypt certbot
 
 # Start Jenkins
 sudo systemctl enable jenkins
