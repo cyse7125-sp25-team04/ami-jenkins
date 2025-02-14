e77f8df07ca7e7ed36de85dc5ec9dc49f4d97763
sri-vijay-kalki <srivijaykalki@gmail.com>
2025-02-14 14:31:25 -0500

    [UDATE]: update script

---
+++ jenkins_scripts/jobs/static-site-image.groovy
@@ -0,0 +1,25 @@
+multibranchPipelineJob('infra-gcp-status-check') {
+    branchSources {
+        github {
+            id('2')
+            repoOwner('cyse7125-sp25-team04')
+            repository('static-site')
+            buildForkPRHead(false)
+            buildForkPRMerge(false)
+            buildOriginBranchWithPR(false)
+            buildOriginBranch(true)
+            scanCredentialsId('git-credentials')
+        }
+    }
+    configure { node ->
+        def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
+            spec('')
+            token("static-site")
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
